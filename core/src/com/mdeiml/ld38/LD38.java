cd3e954f9a10457b4f69d82a62a4991666bea5d5
mdeiml <matthias@deiml.net>
2017-04-22 10:50:07 +0200

    Added waves and background to game

--- core/src/com/mdeiml/ld38/LD38.java
+++ core/src/com/mdeiml/ld38/LD38.java
@@ -3,25 +3,71 @@ package com.mdeiml.ld38;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class LD38 extends ApplicationAdapter {
+
+	private static final float CAM_HEIGHT = 150;
+	private static final float WAVE_SPEED = 3;
+	private static final float WAVE_BOB_HEIGHT = 5;
+	private static final float WAVE_BOB_OFFSET = 1;
+	private static final float WAVE_HEIGHT_OFFSET = 5;
+
 	SpriteBatch batch;
-	Texture img;
+	Texture background;
+	OrthographicCamera cam;
+
+	Texture waves;
+	float waveTimer;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("background.png");
+		float aspect = (float)Gdx.graphics.getWidth() / (float)Gdx.graphics.getHeight();
+		float width = aspect * CAM_HEIGHT;
+		cam = new OrthographicCamera(width, CAM_HEIGHT);
+		cam.position.y = CAM_HEIGHT / 2;
+
+		waves = new Texture("waves.png");
+		waveTimer = 0;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		cam.update();
+		batch.setProjectionMatrix(cam.combined);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		// background
+
+		batch.draw(background, 0, 0);
+
+		// waves
+		waveTimer += Gdx.graphics.getDeltaTime();
+		for(int j = 0; j < 2; j++) {
+			float wavePos = (j % 2 == 0 ? 1 : -1) * (waveTimer % waves.getWidth() * WAVE_SPEED);
+			float waveStart = cam.position.x - cam.viewportWidth / 2f - wavePos;
+			float waveOffset = waveStart % waves.getWidth();
+			if(waveOffset < 0) {
+				waveOffset += waves.getWidth();
+			}
+			waveStart -= waveOffset;
+			float waveEnd = cam.position.x + cam.viewportWidth / 2f - wavePos;
+			int numWaves = (int)((waveEnd - waveStart) / waves.getWidth()) + 1;
+			for(int i = 0; i < numWaves; i++) {
+				float x = waveStart + i * waves.getWidth() + wavePos;
+				float y = (float)(Math.sin(waveTimer + j * WAVE_BOB_OFFSET) * WAVE_BOB_HEIGHT/2 - WAVE_BOB_HEIGHT/2) - j * WAVE_HEIGHT_OFFSET;
+				batch.draw(waves, x, y);
+			}
+		}
+
 		batch.end();
 	}
 }
